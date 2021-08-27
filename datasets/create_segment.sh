b215642162e3b9e62dbda589ee8100f9a7dac1c0
phananh1010@yahoo.com <phananh1010@yahoo.com>
2021-08-27 11:13:54 -0400

    create segments contains script to craate one video segment from video

--- datasets/create_segment.sh
+++ datasets/create_segment.sh
@@ -1,3 +1,3 @@
 #TODO: cut video into segment, input parameters: vid_path, start_time, segment_length, vid_out_path
 #example ./create_segment ./paris.mp4 00:00:10 00:00:01
-ffmpeg -i $1 -filter:v scale=3840:-1 -c:a copy -ss $2 -t $3 -async 1 $4
+ffmpeg -i $1 -filter:v "scale=3840:-1,fps=30" -c:a copy -ss $2 -t $3 -async 1 $4
